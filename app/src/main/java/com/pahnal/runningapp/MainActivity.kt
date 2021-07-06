fd040c0025be7086f86cf1c7bf1eec542f0744e7
fahnaladitia <fahnaladitia07@gmail.com>
2021-07-06 17:53:22 +0800

    Setup Dagger-Hilt

--- app/src/main/java/com/pahnal/runningapp/MainActivity.kt
+++ app/src/main/java/com/pahnal/runningapp/MainActivity.kt
@@ -2,8 +2,16 @@ package com.pahnal.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.pahnal.runningapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/pahnal/runningapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.pahnal.runningapp.utils
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
