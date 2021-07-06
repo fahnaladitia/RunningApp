8f22e58acafa4fbab386f18980e99561afdedecb
fahnaladitia <fahnaladitia07@gmail.com>
2021-07-06 21:18:46 +0800

    Drawing the Running Track on the Map

--- app/src/main/java/com/pahnal/runningapp/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/com/pahnal/runningapp/ui/fragment/TrackingFragment.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
 import com.pahnal.runningapp.R
+import com.pahnal.runningapp.services.Polyline
 import com.pahnal.runningapp.services.TrackingService
 import com.pahnal.runningapp.ui.viewmodels.MainViewModel
+import com.pahnal.runningapp.utils.Constants.ACTION_PAUSE_SERVICE
 import com.pahnal.runningapp.utils.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.pahnal.runningapp.utils.Constants.MAP_ZOOM
+import com.pahnal.runningapp.utils.Constants.POLYLINE_COLOR
+import com.pahnal.runningapp.utils.Constants.POLYLINE_WIDTH
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -19,6 +26,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
+
     private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,11 +36,81 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         mapView.onCreate(savedInstanceState)
         btnToggleRun.setOnClickListener {
             sendCommendToService(ACTION_START_OR_RESUME_SERVICE)
+            toggleRun()
         }
         mapView.getMapAsync {
             map = it
+            addAllPolylines()
+        }
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        TrackingService.isTracking.observe(viewLifecycleOwner, {
+            updateTracking(it)
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, {
+            pathPoints = it
+            addLatestPolyline()
+            moveCameraToUser()
+        })
+    }
+
+    private fun toggleRun() {
+        if(isTracking) {
+            sendCommendToService(ACTION_PAUSE_SERVICE)
+        } else {
+            sendCommendToService(ACTION_START_OR_RESUME_SERVICE)
         }
     }
+
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if(!isTracking) {
+            btnToggleRun.text = ("Start")
+            btnFinishRun.visibility = View.VISIBLE
+        } else {
+            btnToggleRun.text = ("Stop")
+            btnFinishRun.visibility = View.GONE
+
+        }
+    }
+
+    private fun moveCameraToUser() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    private fun addAllPolylines() {
+        for (polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyline() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
 
     private fun sendCommendToService(action: String) {
         Intent(requireContext(), TrackingService::class.java).also {
