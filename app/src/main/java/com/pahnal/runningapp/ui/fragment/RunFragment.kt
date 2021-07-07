dbab2e807305335dd7cb559e89edbc7c5c07f945
fahnaladitia <fahnaladitia07@gmail.com>
2021-07-07 13:25:53 +0800

    Sorting Runs

--- app/src/main/java/com/pahnal/runningapp/ui/fragment/RunFragment.kt
+++ app/src/main/java/com/pahnal/runningapp/ui/fragment/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest.permission.*
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,7 @@ import com.pahnal.runningapp.R
 import com.pahnal.runningapp.adapters.RunAdapter
 import com.pahnal.runningapp.ui.viewmodels.MainViewModel
 import com.pahnal.runningapp.utils.Constants.REQUEST_CODE_LOCATION_PERMISSIONS
+import com.pahnal.runningapp.utils.SortType
 import com.pahnal.runningapp.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
@@ -30,7 +32,30 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
         requestPermission()
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, {
+        when(viewModel.sortType) {
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                when(pos) {
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, {
             runAdapter.submitList(it)
         })
 
---
+++ app/src/main/java/com/pahnal/runningapp/utils/SortType.kt
@@ -0,0 +1,5 @@
+package com.pahnal.runningapp.utils
+
+enum class SortType {
+    DATE, RUNNING_TIME, AVG_SPEED, DISTANCE, CALORIES_BURNED
+}
